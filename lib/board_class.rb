e7e57f2140c5a64ec238628fb1d0730f8ed3b342
benxta4424 <benxtaben@gmail.com>
2024-08-27 19:35:59 +0100

    Added 2 new methods for the Board class: is_legal? and adding_moves
    The first method(is_legal?) checks whether the players's choice is inbounds
    If it is we go return true else we return false(this is important for the testing of the method)
    We first test the method with 2 values,one being inbounds and the other one being out of bounds
    I then write the method with enough code no more-no less
    
    The second method checks the players's choice with the method above
    Then if the move is legal adds a value to the bottom of the row
    The bottom rises by one this way and the next choice goes above this current one
    I again,wrote 2 tests for 2 outcomes:
    when the choice meets the criteria
    when the choice does not meet the criteria

--- lib/board_class.rb
+++ lib/board_class.rb
@@ -2,6 +2,9 @@ class Board
 
     def initialize
         @board=Array.new(6) { Array.new(7,'⬤') }
+
+        @red_circle = "\033[31m⬤\033[0m"
+        @black_circle_yellow = "\033[33m⬤\033[0m"
     end
 
     def display_board
@@ -11,8 +14,20 @@ class Board
         end
     end
 
-    def umple
-        @board[5][0]='M'
+    def is_legal?(row_number)
+        return true if row_number.between?(0,6)
+
+        return false
+    end
+
+    def adding_values(player_choice,row_number)
+        if(is_legal?(row_number))
+            @board[5][row_number]=player_choice
+            return true
+        end
+
+        return false
+        
     end
 
 end
