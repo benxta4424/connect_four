f1c7675e449c88e87e332ff88bf8644c985583b4
benxta4424 <benxtaben@gmail.com>
2024-08-30 23:29:40 +0100

    added 6 different colors for player choices

---
+++ lib/different_colors.rb
@@ -0,0 +1,27 @@
+module DifferentColors
+
+    def red
+        red_circle = "\033[31m⬤\033[0m"
+    end
+
+    def green
+        green_circle = "\033[32m⬤\033[0m"  
+    end
+
+    def yellow
+        yellow_circle = "\033[33m⬤\033[0m"
+    end
+
+    def blue
+        blue_circle = "\033[34m⬤\033[0m"   # Blue
+    end
+
+    def magenta
+        magenta_circle = "\033[35m⬤\033[0m" # Magenta
+    end
+
+    def cyan
+        cyan_circle = "\033[36m⬤\033[0m"   # Cyan
+    end
+    
+end
